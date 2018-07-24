b8b625984b4f10c085c5f2d5fbf744ad23544e8f
Sebastian <sebastian@stryczek.pl>
2018-07-24 19:45:42 +0200

    Correct formating

--- src/components/TypeItem.js
+++ src/components/TypeItem.js
@@ -4,7 +4,7 @@ import './TypeItem.css';
 const TypeItem = ({ name, onItemClick }) => {
   const onClick = () => onItemClick(name);
   return (
-    <div className="card text-center m-2 type-item" onClick={ onClick }>
+    <div className="card text-center m-2 type-item" onClick={onClick}>
       <div className="card-body">
         {name}
       </div>
