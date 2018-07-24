fcbe77029c4162cd44607bf27c777b475b0ec4a5
Sebastian <sebastian@stryczek.pl>
2018-07-24 14:49:03 +0200

    Display pokemon types

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { listTypes } from './services/pokeapiService';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      types: []
+    };
+  }
+  
+  async componentDidMount() {
+    const res = await listTypes();
+    this.setState({
+      types: res.map( x => x.name)
+    })
+  }
+
   render() {
     return (
       <div className="App">
+        {
+          this.state.types.map(
+            (type, i) => (<p key={i}>{type}</p>)
+          )
+        }
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
